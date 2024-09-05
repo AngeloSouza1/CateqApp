39e4af02ab552a7bfa7efb26cd3557f858f4b122
AngeloSouza1 <angeloafdesouza@gmail.com>
2024-09-05 16:26:11 -0300

    chore: ajuste views[home-imagens]

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,8 +1,8 @@
 class PagesController < ApplicationController
   def home
-    # Aqui você pode definir variáveis com dados iniciais que quer mostrar
-    @message = "Bem-vindo à sua aplicação!"
-    @aulas = Aula.all.limit(3)  
-    @postagens = Postagem.all.limit(3)  
+   
+    @message = "Bem-vindo ao CateqApp!"
+    # @aulas = Aula.all.limit(3)  
+    # @postagens = Postagem.all.limit(3)  
   end
 end
