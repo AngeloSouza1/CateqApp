7789084791ce5143a07498dcc85ce8dc304d53d9
AngeloSouza1 <angeloafdesouza@gmail.com>
2024-09-21 17:39:15 -0300

    chore: ajuste card com contagem de dados

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,9 +3,13 @@ class PagesController < ApplicationController
   def home
     # Buscar as notificações do usuário logado, caso exista
     if user_signed_in?
-      @notifications = current_user.notifications.order(created_at: :desc).limit(5)
+      @notifications = current_user.notifications.order(created_at: :desc).limit(7)
+      @total_postagens = current_user.postagems.count
+      @total_comentarios = current_user.comentarios.count
+      @total_aulas = Aula.count
     else
       @notifications = []
+   
     end
   end
 end
